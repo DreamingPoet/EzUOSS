70beb50fbbd1fbe70d0291c0a3cb3dfe1a07b657
dreamingpoet <452346377@qq.com>
2022-03-09 17:44:42 +0800

    new plugin

---
+++ Source/EzUOSS/EzUOSS.Build.cs
@@ -0,0 +1,103 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class EzUOSS : ModuleRules
+{
+
+    protected virtual bool bPlatformSupportsWinHttp
+    {
+        get
+        {
+            return Target.Platform.IsInGroup(UnrealPlatformGroup.Windows);
+        }
+    }
+
+    protected virtual bool bPlatformSupportsLibCurl
+    {
+        get
+        {
+            return Target.Platform.IsInGroup(UnrealPlatformGroup.Windows) ||
+                Target.IsInPlatformGroup(UnrealPlatformGroup.Unix) ||
+                Target.IsInPlatformGroup(UnrealPlatformGroup.Android) ||
+                Target.Platform == UnrealTargetPlatform.Switch;
+        }
+    }
+
+    protected virtual bool bPlatformRequiresOpenSSL
+    {
+        get
+        {
+            return Target.Platform.IsInGroup(UnrealPlatformGroup.Windows) ||
+                Target.IsInPlatformGroup(UnrealPlatformGroup.Unix) ||
+                Target.IsInPlatformGroup(UnrealPlatformGroup.Android);
+        }
+    }
+
+    public EzUOSS(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicDefinitions.Add("HTTP_PACKAGE=1");
+
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// 要包含的路径
+			}
+			);
+
+		PublicDependencyModuleNames.AddRange(
+			new string[] {
+				"Core"
+			}
+			);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[] {
+				"SSL",
+				"Engine"
+			}
+			);
+
+		if (bPlatformSupportsLibCurl)
+		{
+			AddEngineThirdPartyPrivateStaticDependencies(Target, "libcurl");
+			PrivateDependencyModuleNames.AddRange(
+				new string[] {
+					"Sockets",
+				}
+			);
+
+			PublicDefinitions.Add("CURL_ENABLE_DEBUG_CALLBACK=1");
+			if (Target.Configuration != UnrealTargetConfiguration.Shipping)
+			{
+				PublicDefinitions.Add("CURL_ENABLE_NO_TIMEOUTS_OPTION=1");
+			}
+		}
+		else
+		{
+			PublicDefinitions.Add("WITH_LIBCURL=0");
+		}
+
+		// Use Curl over WinHttp on platforms that support it (until WinHttp client security is in a good place at the least)
+		if (bPlatformSupportsWinHttp)
+		{
+			AddEngineThirdPartyPrivateStaticDependencies(Target, "WinHttp");
+			PublicDefinitions.Add("WITH_WINHTTP=1");
+		}
+		else
+		{
+			PublicDefinitions.Add("WITH_WINHTTP=0");
+		}
+
+		if (bPlatformRequiresOpenSSL)
+		{
+			AddEngineThirdPartyPrivateStaticDependencies(Target, "OpenSSL");
+		}
+
+		if (Target.Platform == UnrealTargetPlatform.IOS || Target.Platform == UnrealTargetPlatform.TVOS || Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			PublicFrameworks.Add("Security");
+		}
+	}
+}
---
+++ Source/EzUOSS/Private/EzUOSS.cpp
@@ -0,0 +1,41 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EzUOSS.h"
+#include "HttpModule.h"
+#include "Interfaces/IHttpRequest.h"
+
+#define LOCTEXT_NAMESPACE "FEzUOSSModule"
+
+void FEzUOSSModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+
+}
+
+void FEzUOSSModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+
+	/*
+		// 创建Http 请求
+	TSharedRef<IHttpRequest> Request = FHttpModule::Get().CreateRequest();
+	// 设置请求头
+	Request->SetHeader("Content-Type", "text/javascript;charset=utf-8");
+	// 设置请求方式
+	Request->SetVerb("POST");
+	// 请求的链接
+	Request->SetURL("192.168.5.21:8000/test_ue4"); // 服务端预留的测试接口
+	// 内容包
+	//Request->SetContentAsString(server_data);
+	// 设置回调函数
+	Request->OnProcessRequestComplete().BindUObject(this, &UNetWidget::RequestComplete);
+	// 发送请求
+	Request->ProcessRequest();
+*/
+}
+
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FEzUOSSModule, EzUOSS)
